29e3b519c2ce7576246c718ca956319232a18e60
Your Name <you@example.com>
2024-11-10 21:42:11 -0800

    hero ok

--- datascrap/tailwind.config.js
+++ datascrap/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "source-sans": ["Source Sans 3", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        
+      },
+      boxShadow: {
+        navbar:
+          "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        "scroll-loop": "scroll 20s linear infinite",
+      },
+    },
   },
   plugins: [],
-}
-
+};
